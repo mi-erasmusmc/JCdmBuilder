cc8b375c4867c9845d2678b13bb0798c488eb1ff
bmosseveld <bmosseveld@10.130.2.243>
2020-02-24 13:30:09 +0100

    Version 0.3.7

--- src/org/ohdsi/jCdmBuilder/JCdmBuilderMain.java
+++ src/org/ohdsi/jCdmBuilder/JCdmBuilderMain.java
@@ -74,7 +74,7 @@ import org.ohdsi.utilities.PropertiesManager;
 import org.ohdsi.utilities.StringUtilities;
 
 public class JCdmBuilderMain {
-	public static final String VERSION = "0.3.6";
+	public static final String VERSION = "0.3.7";
 	
 	private static final String ICON = "/org/ohdsi/jCdmBuilder/OHDSI Icon Picture 048x048.gif"; 
 	
