d3f845ff7f9b4f5504f93fbc15d78187b414baf9
Mees Mosseveld <b.mosseveld@erasmusmc.nl>
2026-08-31 16:11:45 +0200

    Added CDM version 5.5.0.

--- src/org/ohdsi/jCdmBuilder/JCdmBuilder.java
+++ src/org/ohdsi/jCdmBuilder/JCdmBuilder.java
@@ -92,7 +92,7 @@ import org.ohdsi.utilities.StringUtilities;
 import org.ohdsi.utilities.files.IniFile;
 
 public class JCdmBuilder {
-	public static final String VERSION = "5.4.2.1";
+	public static final String VERSION = "5.5.0.0";
 
 	private static final String ICON = "/org/ohdsi/jCdmBuilder/OHDSI Icon Picture 048x048.gif";
 
